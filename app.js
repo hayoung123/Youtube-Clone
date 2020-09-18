2f798fe9792102cc81b75d08afc17866e43dc5aa
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-09-18 15:13:27 +0900

    make router_folder

--- app.js
+++ app.js
@@ -7,18 +7,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () => {
-  console.log(`Listening on: http://localhost:${PORT}`);
-};
-
-const handleHome = (req, res) => res.send("Hello world");
-
-const handleProfile = (req, res) => res.send("Here is profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -26,8 +19,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
-app.get("/profile", handleProfile);
-
-app.listen(PORT, handleListening);
+export default app;
