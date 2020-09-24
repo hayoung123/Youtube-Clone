f3665a0ea3d082f79d4c7af45a2078b8f3e6a29d
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-09-24 21:54:29 +0900

    create form pages - join, login, edit-profile

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Youtube Clone
 
 Cloning Youtube with Vanilla and NodsJS
+
+## Pages :
+
+- [ ] Home
+- [ ] Join
+- [x] Login
+- [ ] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -10,8 +10,9 @@ import routes from "../routes";
 const userRouter = express.Router();
 
 userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
+//userDetail이 위에 있으면 :id 때문에 /edit-profile 를 아이디로 인식한다.
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
