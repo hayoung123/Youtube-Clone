6c7fc6bafbc5cfdef66222b8d085dbf90a95d866
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2020-09-28 22:50:53 +0900

    install mongo DB

--- db.js
+++ db.js
@@ -1,41 +1,18 @@
-export const videos = [
-  {
-    id: 1234,
-    title: "Video test",
-    description: "This is description",
-    views: 10,
-    videoFile:
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-    creator: {
-      id: "proshy",
-      name: "hayoung",
-      email: "hayoung@google.com",
-    },
-  },
-  {
-    id: 12345,
-    title: "Video test1",
-    description: "This is description1",
-    views: 10,
-    videoFile:
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-    creator: {
-      id: "proshy",
-      name: "hayoung",
-      email: "hayoung@google.com",
-    },
-  },
-  {
-    id: 123456,
-    title: "Video test2",
-    description: "This is description2",
-    views: 10,
-    videoFile:
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-    creator: {
-      id: "proshy",
-      name: "hayoung",
-      email: "hayoung@google.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/youtube-clone", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅Connected to DB");
+const handleError = () => console.log(`❌ Error on DB connection:${error}`);
+
+//db가 open 됐을 때 확인하기 위해 console.log 콜백함수 실행
+db.once("open", handleOpen);
+//error 시 error 출력하는 콜백함수
+db.on("error", handleError);
+
+//import를 하지 않으면 db는 open 되지 않는다.
